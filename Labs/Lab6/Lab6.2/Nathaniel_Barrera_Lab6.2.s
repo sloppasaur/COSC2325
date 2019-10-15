360fce58bb238fd9ce1f57c069781f2d54199c5a
Nathaniel Barrera <nathaniel.barrera39@gmail.com>
2019-10-15 11:57:31 -0500

    Cleaned things up

--- Labs/Lab6/Lab6.2/Nathaniel_Barrera_Lab6.2.s
+++ Labs/Lab6/Lab6.2/Nathaniel_Barrera_Lab6.2.s
@@ -11,12 +11,11 @@ main:
   bl exit
 
 write:
-
   mov x1, #10
   mov x2, #20
   ldr x1, [sp, #8]    // Added code
   ldr x2, [sp, #0]    // Added code
-  add sp, sp, #16     // Added code 
+  add sp, sp, #16     // Added code
   add x3, x1, x2
   ldr x0, =msg
   bl printf
